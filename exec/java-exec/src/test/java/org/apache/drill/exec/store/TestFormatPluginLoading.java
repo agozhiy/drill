8cdb04877f2374af24673d6f03a6f0d389e6c634
Anton Gozhiy <anton5813@gmail.com>
2019-05-06 19:29:54 +0300

    DRILL-7030: Moved the test bootstrap files to avoid cluttering the classpath.

--- exec/java-exec/src/test/java/org/apache/drill/exec/store/TestFormatPluginLoading.java
+++ exec/java-exec/src/test/java/org/apache/drill/exec/store/TestFormatPluginLoading.java
@@ -19,11 +19,17 @@
 package org.apache.drill.exec.store;
 
 import org.apache.drill.common.logical.StoragePluginConfig;
+import org.apache.drill.exec.ExecConstants;
 import org.apache.drill.exec.ExecTest;
 import org.apache.drill.exec.store.dfs.FileSystemConfig;
 import org.apache.drill.exec.store.easy.text.TextFormatPlugin;
 import org.apache.drill.exec.store.sys.PersistentStore;
+import org.apache.drill.shaded.guava.com.google.common.io.Resources;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.io.File;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -32,10 +38,45 @@ public class TestFormatPluginLoading extends ExecTest {
   private final StoragePluginRegistryImpl storagePluginRegistry;
   private final PersistentStore<StoragePluginConfig> plugins;
 
+  private static File storagePlugins;
+  private static File formatPlugins;
+
   public TestFormatPluginLoading() throws Exception {
     storagePluginRegistry = new StoragePluginRegistryImpl(mockDrillbitContext());
     storagePluginRegistry.init();
     plugins = storagePluginRegistry.getStore();
+  }
+
+  @BeforeClass
+  public static void setup() {
+    storagePlugins = new File(Resources.getResource("store/" + ExecConstants.BOOTSTRAP_STORAGE_PLUGINS_FILE).getFile());
+    formatPlugins = new File(Resources.getResource("store/" + ExecConstants.BOOTSTRAP_FORMAT_PLUGINS_FILE).getFile());
+    storagePlugins = rename(storagePlugins);
+    formatPlugins = rename(formatPlugins);
+  }
+
+  @AfterClass
+  public static void tearDown() {
+    renameBack(storagePlugins);
+    renameBack(formatPlugins);
+  }
+
+  private static File rename(File file) {
+    File newFile = file.getParentFile()
+        .getParentFile()
+        .toPath()
+        .resolve(file.getName())
+        .toFile();
+    return file.renameTo(newFile) ? newFile : null;
+  }
+
+  private static void renameBack(File file) {
+    File newFile = file.getParentFile()
+        .toPath()
+        .resolve("store")
+        .resolve(file.getName())
+        .toFile();
+    file.renameTo(newFile);
   }
 
   @Test
